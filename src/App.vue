3478531c9d14bc0afa488542efdbbbc906a48b1f
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-04-20 17:32:19 +0600

    Buttons markup

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
         <div class="container">
             <Navbar />
             <Display />
-            <div class="buttons-container"></div>
+            <div class="buttons-container">
+                <Button
+                    v-for="button in buttonList"
+                    :key="button.index"
+                    :button="button"
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -11,15 +17,38 @@
 <script>
 import Navbar from "./components/Navbar.vue";
 import Display from "./components/Display.vue";
+import Button from "./components/Button.vue";
 
 export default {
     name: "App",
     data() {
-        return {};
+        return {
+            buttonList: [
+                { index: 1, data: "7" },
+                { index: 2, data: "8" },
+                { index: 3, data: "9" },
+                { index: 4, data: "DEL" },
+                { index: 5, data: "4" },
+                { index: 6, data: "5" },
+                { index: 7, data: "6" },
+                { index: 8, data: "+" },
+                { index: 9, data: "1" },
+                { index: 10, data: "2" },
+                { index: 11, data: "3" },
+                { index: 12, data: "-" },
+                { index: 13, data: "." },
+                { index: 14, data: "0" },
+                { index: 15, data: "/" },
+                { index: 16, data: "x" },
+                { index: 17, data: "RESET" },
+                { index: 18, data: "=" },
+            ],
+        };
     },
     components: {
         Navbar,
         Display,
+        Button,
     },
     methods: {
         // write your methods here
@@ -40,5 +69,11 @@ export default {
     height: 700px;
     border-radius: 10px;
     padding: 8px;
+}
+
+.buttons-container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
 }
 </style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,30 +1,55 @@
 <template>
-  <div>
-    <!-- write your code here -->
-    
-  </div>
+    <div class="button">
+        <p>{{ button.data }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data() {
-    return {
-      // write your data here
-    };
-  },
-  methods: {
-    // write your methods here
-  },
-  computed: {
-    // write your computed properties here
-  },
-  mounted() {
-    // write your code here
-  }
+    name: "App",
+    data() {
+        return {
+        };
+    },
+    props: {
+        button: {
+            type: Object,
+            default: {
+                index: 1,
+                data: "0",
+            },
+        },
+    },
+    methods: {
+        // write your methods here
+    },
+    computed: {
+        // write your computed properties here
+    },
+    mounted() {
+        // write your code here
+    },
 };
 </script>
 
 <style>
 /* write your styles here */
+.button {
+    width: 110px;
+    height: 70px;
+    background: #3a3a3a;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 12px;
+    box-shadow: 0 5px 0 0 #000000;
+    margin: 8px;
+    cursor: pointer;
+}
+
+.button p {
+    color: #fff;
+    font-size: 32px;
+    font-weight: 700;
+}
 </style>
